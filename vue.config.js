4267d14cd9edaf1e256e3269758e35338baa4b53
john <t69685186@gmail.com>
2023-08-15 10:44:05 +0800

    add deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-export default defineConfig({
-  base: '/demo',
-  plugins: [
-    vue()
-  ]
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/demo/' // 為 repo 名稱
+    : '/'
+}
